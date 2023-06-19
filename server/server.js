a49ac73c817ab39a19ae39f45ad25ef7cddc3783
PREETI <preeti@MacBook-Air.local>
2023-06-19 11:11:23 +0530

    basic cleanup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,36 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
+const path = require('path');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const publicPath = '/';
+let outputPath;
+outputPath = path.resolve(process.cwd(), 'build');
+app.use(publicPath, loopback.static(outputPath));
+
+const crossMiddleware = cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+      'Origin',
+      'Content-type',
+      'Content-Length',
+      'Authorization',
+      'Accept',
+      'X-Requested-With'],
+  credentials: true,
+  preflightContinue: true,
+  optionsSuccessStatus: 200
+});
+app.use(crossMiddleware);
+app.use('*', crossMiddleware);
+app.get('/home', (req, res) => {
+  res.sendFile(path.resolve(outputPath, 'index.html'));
+});
 
 app.start = function() {
   return app.listen(function() {
